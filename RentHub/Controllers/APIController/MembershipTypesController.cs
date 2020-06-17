0808dbe8382d373e919feac36d246cffcad5d4be
Susan Joshi <susan.joshi@aggrowth.com>
2020-06-16 23:19:06 -0500

    Changes the return type to IHttpActionResult.

--- RentHub/Controllers/APIController/MembershipTypesController.cs
+++ RentHub/Controllers/APIController/MembershipTypesController.cs
@@ -22,26 +22,26 @@ namespace RentHub.Controllers.APIController
 
         // GET /api/membershiptypes
         [HttpGet]
-        public IEnumerable<MembershipTypeDto> GetMembershipTypes()
+        public IHttpActionResult GetMembershipTypes()
         {
-            return _context.MembershipTypes.ToList().Select(Mapper.Map<MembershipType, MembershipTypeDto>);
+            return Ok(_context.MembershipTypes.ToList().Select(Mapper.Map<MembershipType, MembershipTypeDto>));
         }
 
         // GET /api/membershiptypes/1
         [HttpGet]
-        public MembershipTypeDto GetMembershipType(byte id)
+        public IHttpActionResult GetMembershipType(byte id)
         {
             var membershipType = _context.MembershipTypes.SingleOrDefault(m => m.Id == id);
 
-            if(membershipType == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+            if (membershipType == null)
+                return NotFound();
 
-            return Mapper.Map<MembershipType, MembershipTypeDto>(membershipType);
+            return Ok(Mapper.Map<MembershipType, MembershipTypeDto>(membershipType));
         }
 
         // POST /api/membershiptypes
         [HttpPost]
-        public MembershipTypeDto CreateMembershipType(MembershipTypeDto membershipTypeDto)
+        public IHttpActionResult CreateMembershipType(MembershipTypeDto membershipTypeDto)
         {
             if(!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -50,12 +50,12 @@ namespace RentHub.Controllers.APIController
             _context.MembershipTypes.Add(membershipType);
             _context.SaveChanges();
 
-            return membershipTypeDto;
+            return Created(new Uri(Request.RequestUri + "/" + membershipType.Id), membershipTypeDto);
         }
 
         // PUT /api/membershiptypes/1
         [HttpPut]
-        public MembershipTypeDto UpdateMembershipType(byte id, MembershipTypeDto membershipTypeDto)
+        public IHttpActionResult UpdateMembershipType(byte id, MembershipTypeDto membershipTypeDto)
         {
             if(!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -69,12 +69,12 @@ namespace RentHub.Controllers.APIController
 
             _context.SaveChanges();
 
-            return membershipTypeDto;
+            return Ok(membershipTypeDto);
         }
 
         // DELETE /api/membershiptypes/1
         [HttpDelete]
-        public MembershipType DeleteMembershipType(byte id)
+        public IHttpActionResult DeleteMembershipType(byte id)
         {
             if (!ModelState.IsValid)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
@@ -87,7 +87,7 @@ namespace RentHub.Controllers.APIController
             _context.MembershipTypes.Remove(membershipTypeInDb);
             _context.SaveChanges();
 
-            return membershipTypeInDb;
+            return Ok(membershipTypeInDb);
         }
 
 
